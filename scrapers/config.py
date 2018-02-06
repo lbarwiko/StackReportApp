92d4a2ff652ffa96f9a7e9716d68d64e381452f3
David A. Sokol <dasokol@umich.edu>
2018-02-06 18:37:34 -0500

    create data scrapers

---
+++ scrapers/config.py
@@ -0,0 +1,11 @@
+"""
+config file for global variables
+"""
+
+# TODO this is bad security practice
+# alphavantage api key
+API_KEY = "U8RRYQ7SOSDVDWVW"
+
+# alphavantage base url
+ALPHA_BASE_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
+
