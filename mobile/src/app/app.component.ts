38b07573f7fcd2aa57658763d991046a312640d0
Luca409 <lloncar@umich.edu>
2018-03-20 19:14:30 -0400

    security is root page

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
+import { SecurityPage } from '../pages/security/security';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = SecurityPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
