49ab329db50b82e1b487696b4c8a3dc8713be9c3
lbarwiko <lbarwiko@umich.edu>
2018-03-21 13:36:19 -0400

    Made servers ready to deploy.

--- restApi/Gulpfile.js
+++ restApi/Gulpfile.js
@@ -7,26 +7,30 @@ var prefixDist = 'dist';
 function setupConfig(prefix){
 	gulp.src(['./config/' + prefix + '_config.js', './config/server_config.js'])
 	.pipe(concat('server_config.js'))
-	.pipe(gulp.dest('./' + prefix + '/'));
+	.pipe(gulp.dest('./src/'));
 }
 
 gulp.task('build-dev', function(){
 	setupConfig('src');
 })
 
-gulp.task('build', function(){
+gulp.task('build-prod', function(){
+	setupConfig('dist');
+})
 
-	setupConfig(prefixDev);
-	setupConfig(prefixDist);
+// gulp.task('build', function(){
 
-	//Copy app folder
-	gulp.src('./' + prefixDev + '/app/**/*')
-	.pipe(gulp.dest('./' + prefixDist + '/app/'));
+// 	setupConfig(prefixDev);
+// 	setupConfig(prefixDist);
 
-	//Copy server file
-	gulp.src('./' + prefixDev + '/')
-	.pipe(gulp.dest('./' + prefixDist + '/'));
-});
+// 	//Copy app folder
+// 	gulp.src('./' + prefixDev + '/app/**/*')
+// 	.pipe(gulp.dest('./' + prefixDist + '/app/'));
+
+// 	//Copy server file
+// 	gulp.src('./' + prefixDev + '/')
+// 	.pipe(gulp.dest('./' + prefixDist + '/'));
+// });
 
 gulp.task("watch", function(){
 	console.log("Watching for changes to config");
--- restApi/config/dist_config.js
+++ restApi/config/dist_config.js
@@ -1,15 +1,20 @@
-const dist_config = {
-    prefix: 'dist',
+const dev_config = {
+    prefix: 'src',
     db:{
 	    host: 'localhost', // 'localhost' is the default;
 	    port: 5432, // 5432 is the default;
 	    database: 'stackreport',
     },
     auth:{
-        secret: '4CXzlStbG9OLDeRguS583Bqn1HZ2xpP7'
+        secret: 'lkas3l2kj42l3k4jamq22114',
+        jwtSession: {
+            session: false
+        },
+        expiresIn: '90d', // Expires in Days
+        saltRounds: 10
     },
-    port:80,
-    sslport:443
+    port:8080,
+    sslport:4443
 }
 
-let data = dist_config;
+let data = dev_config;
