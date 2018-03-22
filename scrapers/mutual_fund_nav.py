859ca3e946298456c0a16bdb72e3c664d6108b46
Roy <roych@umich.edu>
2018-03-22 10:28:35 -0400

    add stock historical from yahoo finance

--- scrapers/mutual_fund_nav.py
+++ scrapers/mutual_fund_nav.py
@@ -54,6 +54,28 @@ def upload_mf_historical(ticker):
 
 	add_tuple_mf_history(tuple_list)
 
+def upload_stock_historical(ticker):
+
+	result = get_quote_historical(ticker)
+	# convert into tuple list ((m_symbol, m_date, price), ...)
+	tuple_list = []
+
+	for each in result:
+		if "date" in each and "close" in each:
+			if each["date"] is not None and each["close"] is not None:
+				date = time.strftime("%Y%m%d", time.gmtime(float(each["date"])))
+				try:
+					tup = (ticker, date, float(each["close"]))
+				except TypeError:
+					print (each)
+
+				tuple_list.append(tup)
+
+	tuple_list = list(set(tuple_list))
+
+
+	add_tuple_stock_history(tuple_list)	
+
 
 
 def get_quote(ticker):
