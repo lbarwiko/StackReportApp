a12a595cd003f08e8362b302c7ecc2c01edd8a07
David A. Sokol <dasokol@umich.edu>
2018-03-21 23:39:08 -0400

    fix post

--- predictions/post.py
+++ predictions/post.py
@@ -2,8 +2,9 @@
 module for submitting post requests of results to the restAPI
 """
 
-import requests
+import os
 import sys
+import json
 sys.path.append(sys.path[0]+"/../")
 from predictions_database.helper import get_mf_list
 
@@ -17,7 +18,7 @@ def load_data():
 
 	for mf_symbol in mf_symbols:
 		with open("/root/StackReport/predictions/" + mf_symbol + "_regr.json") as file:
-			data = file.read()
+			data = json.dumps(json.load(file))
 			mf_symbols_to_data[mf_symbol] = data
 
 	return mf_symbols_to_data
@@ -27,8 +28,10 @@ def post_request(mf_symbol, data):
 	send prediction results for mf_symbol to server
 	"""
 	url = "http://www.stackreport.io:80/api/p/"
-	r = requests.post(url, data)
+	response = os.popen("curl --request POST --url " + url + " --header 'Content-Type: application/json' --data '" + data + "'").read()
 
+	return response
+	
 def main():
 	mf_symbols_to_data = load_data()
 
