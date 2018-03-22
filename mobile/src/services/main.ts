64de417a70485b179ba2a4d50a3d517ecd49af8b
Luca409 <lloncar@umich.edu>
2018-03-22 04:28:49 -0400

    fixed merge conflict in services main

--- mobile/src/services/main.ts
+++ mobile/src/services/main.ts
@@ -3,11 +3,8 @@ import { AuthService } from './auth.service';
 import { FundService } from './fund.service';
 import { EndpointService } from './endpoint.service';
 import { FollowingService } from './following.service';
-<<<<<<< HEAD
 import { PredictionService } from './prediction.service';
-=======
 import { SecurityService } from './security.service';
->>>>>>> 14f1c12d2f8ed63008a08910e35d309d82424359
 
 export {
     UserService,
@@ -15,9 +12,6 @@ export {
     FundService,
     EndpointService,
     FollowingService,
-<<<<<<< HEAD
-    PredictionService
-=======
+    PredictionService,
     SecurityService
->>>>>>> 14f1c12d2f8ed63008a08910e35d309d82424359
 }
