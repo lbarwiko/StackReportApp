3ec5bd83ed40e8434fa9d1f408b6588bc2730fde
David A. Sokol <dasokol@umich.edu>
2018-03-21 20:51:00 -0400

    fix post.py to load from db

--- predictions/post.py
+++ predictions/post.py
@@ -9,9 +9,10 @@ def load_data():
 	"""
 	return dictionary mapping mf_symbols to their predicted data
 	"""
-
-	#TODO load mf symbols list and prediction data from db
+	# load mf_symbols from db
+	mf_symbols = get_mf_list()
 	mf_symbols_to_data = {}
+
 	for mf_symbol in mf_symbols:
 		with open("~/StackReport/predictions/" + mf_symbol + "_regr.json") as file:
 			data = json.load(file)
@@ -23,7 +24,6 @@ def post_request(mf_symbol, data):
 	"""
 	send prediction results for mf_symbol to server
 	"""
-
 	url = "localhost:8000/api/p/"
 	r = requests.post(url, data)
 
