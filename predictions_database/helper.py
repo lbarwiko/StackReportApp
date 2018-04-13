305a12372ede1b5db12ed7fbc12a9a0b2a93b3b8
David A. Sokol <dasokol@umich.edu>
2018-04-12 22:48:38 -0400

    fix Roy's get_children bug

--- predictions_database/helper.py
+++ predictions_database/helper.py
@@ -528,8 +528,7 @@ def get_children(ticker):
 """
 def get_children(ticker):
     cur = db_cursor()
-    op_string = ("""SELECT C2.child_symbol FROM children C1 JOIN children C2 ON C1.parent_symbol = C2.parent_s
-ymbol WHERE C1.child_symbol = '%s'""" % ticker)
+    #op_string = ("""SELECT C2.child_symbol FROM children C1 JOIN children C2 ON C1.parent_symbol = C2.parent_symbol WHERE C1.child_symbol = '%s'""" % ticker)
 
     try:
         cur.execute(op_string)
