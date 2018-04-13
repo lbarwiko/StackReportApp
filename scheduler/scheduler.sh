9d20d1b529be2bde25322b35917a2812f3513fda
David A. Sokol <dasokol@umich.edu>
2018-04-13 18:58:34 -0400

    fix check if markets were open yesterday bug

--- scheduler/scheduler.sh
+++ scheduler/scheduler.sh
@@ -12,7 +12,7 @@ fi
 
 # if open yesterday, run predictions and post to front end
 result2=$(curl -sL "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo" | grep -sc $(date -d '1 day ago' +%F))
-if [[ "$result2" == "2" ]]
+if [[ "$result2" == "1" ]]
 then
 	# python3 ~/StackReport/predictions/regression.py >> ~/StackReport/logs/regression.log 2>&1
 	# use prev date since we don't get current day nav data right away
